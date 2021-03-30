70355e2b228154855d8cf2c342b8b559f72ef646
Samuel Fox <samueljasonfox@gmail.com>
2021-03-30 11:06:32 -0700

    update import paths for app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 import AllClients from "./Pages/AllClients";
 import Locations from "./Pages/Locations";
 import "./style.css";
-import user from "./APIs/user";
+import user from "./utils/APIs/user";
 
 function App() {
   const [search, setSearch] = useState(""); // search form inputs
